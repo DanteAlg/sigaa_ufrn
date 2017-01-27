4b01aaaa5df49fe29061e66afc2df34358ccae2d
Dante Santos <dante.alighierimds@gmail.com>
2017-01-27 01:50:33 -0300

    Require sigaa ufrn exam file

--- lib/sigaa_ufrn.rb
+++ lib/sigaa_ufrn.rb
@@ -4,6 +4,7 @@ require "sigaa_ufrn/authentication"
 # Services
 require "sigaa_ufrn/sigaa_base"
 require "sigaa_ufrn/library"
+require "sigaa_ufrn/exam"
 
 module SigaaUfrn
   # Your code goes here...

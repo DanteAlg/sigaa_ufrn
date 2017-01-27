beda298cfc0e9f09590bf9b096d3c990411ee30b
Dante Santos <dante.alighierimds@gmail.com>
2017-01-27 01:22:42 -0300

    Add library spec

---
+++ spec/sigaa_ufrn/library_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+
+RSpec.describe SigaaUfrn::Library do
+  describe '#materials' do
+  end
+
+  describe '#libraries' do
+  end
+
+  describe '#articles' do
+  end
+
+  describe '#detailed_articles' do
+  end
+
+  describe '#books_loan' do
+  end
+
+  describe '#books_loan' do
+  end
+
+  describe '#current_books_loan' do
+  end
+
+  describe '#books' do
+  end
+
+  describe '#find_by_material_in_library' do
+  end
+end
